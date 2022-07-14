0fbe76e3cc81756a84dea438c45386c3d42223a0
WolfAURman <furry.wolf.alone13@gmail.com>
2022-07-15 04:37:02 +0600

    Adding garbage removal

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-chown -R git:git /
+## Creating a dump
+chown -R git:git ~/
 chown -R git:git /usr
 su git -c "gitea dump -c /etc/gitea/app.ini"
 mkdir ~/dump
 cp gitea-dump-*.zip ~/dump
 
-##
+## Downloading and updating to the current version
 systemctl disable gitea.service
 systemctl stop gitea.service
 wget -O gitea https://dl.gitea.io/gitea/1.16.9/gitea-1.16.9-linux-amd64
@@ -15,3 +16,7 @@ rm -rf /usr/local/bin/gitea
 cp gitea /usr/local/bin/gitea
 systemctl enable gitea.service
 systemctl start gitea.service
+
+## Garbage removal
+rm -rf gitea
+rm -rf gitea-dump-*.zip
