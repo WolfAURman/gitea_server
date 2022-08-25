d34bd9159be012a04aa025fe9d47c9af6f3b4c28
WolfAURman <furry.wolf.alone13@gmail.com>
2022-08-25 06:38:46 +0600

    Well, I've finally rethought this script. Added support for fedora, AlmaLinux, CentOS (#3)

--- install.sh
+++ install.sh
@@ -1,23 +1,108 @@
 #!/bin/bash
+## Section for Debian
 
-apt update && apt upgrade -y
-wget -O gitea https://dl.gitea.io/gitea/1.16.9/gitea-1.16.9-linux-amd64
-chmod +x gitea
-adduser \
-   --system \
-   --shell /bin/bash \
-   --gecos 'Git Version Control' \
-   --group \
-   --disabled-password \
-   --home /home/git \
-   git
-mkdir -p /var/lib/gitea/{custom,data,log}
-chown -R git:git /var/lib/gitea/
-chmod -R 750 /var/lib/gitea/
-mkdir /etc/gitea
-chown root:git /etc/gitea
-chmod 770 /etc/gitea
-cp gitea /usr/local/bin/gitea
-cp gitea.service /etc/systemd/system
-systemctl enable gitea
-systemctl start gitea
+if grep 'Debian GNU/Linux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
+
+then
+
+	apt update && apt upgrade -y
+	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	chmod +x gitea
+	adduser \
+	   --system \
+	   --shell /bin/bash \
+	   --gecos 'Git Version Control' \
+	   --group \
+	   --disabled-password \
+	   --home /home/git \
+	   git
+	mkdir -p /var/lib/gitea/{custom,data,log}
+	chown -R git:git /var/lib/gitea/
+	chmod -R 750 /var/lib/gitea/
+	mkdir /etc/gitea
+	chown root:git /etc/gitea
+	chmod 770 /etc/gitea
+	cp gitea /usr/local/bin/gitea
+	cp gitea.service /etc/systemd/system
+	systemctl enable gitea
+	systemctl start gitea
+
+fi
+
+## Section for Fedora
+
+if grep 'Fedora Linux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
+
+then
+
+	dnf update -y && dnf install wget -y
+	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	chmod +x gitea
+	adduser \
+	   --shell /bin/bash \
+	   --home /home/git \
+	   git
+	mkdir -p /var/lib/gitea/{custom,data,log}
+	chown -R git:git /var/lib/gitea/
+	chmod -R 750 /var/lib/gitea/
+	mkdir /etc/gitea
+	chown root:git /etc/gitea
+	chmod 770 /etc/gitea
+	cp gitea /usr/local/bin/gitea
+	cp gitea.service /etc/systemd/system
+	systemctl enable gitea
+	systemctl start gitea
+
+fi
+
+## Section for AlmaLinux
+
+if grep 'AlmaLinux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
+
+then
+
+	dnf update -y && dnf install wget -y
+	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	chmod +x gitea
+	adduser \
+	   --shell /bin/bash \
+	   --home /home/git \
+	   git
+	mkdir -p /var/lib/gitea/{custom,data,log}
+	chown -R git:git /var/lib/gitea/
+	chmod -R 750 /var/lib/gitea/
+	mkdir /etc/gitea
+	chown root:git /etc/gitea
+	chmod 770 /etc/gitea
+	cp gitea /usr/local/bin/gitea
+	cp gitea.service /etc/systemd/system
+	systemctl enable gitea
+	systemctl start gitea
+
+fi
+
+## Section for CentOS
+
+if grep 'CentOS' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
+
+then
+
+	{dnf update -y && dnf install wget -y
+	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	chmod +x gitea
+	adduser \
+	   --shell /bin/bash \
+	   --home /home/git \
+	   git
+	mkdir -p /var/lib/gitea/{custom,data,log}
+	chown -R git:git /var/lib/gitea/
+	chmod -R 750 /var/lib/gitea/
+	mkdir /etc/gitea
+	chown root:git /etc/gitea
+	chmod 770 /etc/gitea
+	cp gitea /usr/local/bin/gitea
+	cp gitea.service /etc/systemd/system
+	systemctl enable gitea
+	systemctl start gitea
+
+fi
