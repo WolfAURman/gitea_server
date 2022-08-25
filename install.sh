1d2056f56acd1eacfeb4d40972f28995c88c8036
WolfAURman <furry.wolf.alone13@gmail.com>
2022-08-26 04:52:43 +0600

    Update. Added version selection (#4)

--- install.sh
+++ install.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
+##
+clear
+##
+echo "|========================================|"
+echo "|               Hey! Hello!              |"
+echo "|   Have you decided to install gitea    |"
+echo "|             on your server             |"
+echo "|  This script will help you with this   |"
+echo "|         Answer a few questions         |"
+echo "|   and then wait for the installation   |"
+echo "|========================================|"
+echo "|             Successful use!            |"
+echo "|========================================|"
+echo "|   The installation will start in 10s   |"
+echo "|========================================|"
+##
+sleep 10
+##
+clear
+##
+echo "|========================================|"
+echo "|  Which version do you want to install? |"
+echo "|      These versions are available:     |"
+echo "|========================================|"
+echo "|                 1.16.7                 |"
+echo "|                 1.16.8                 |"
+echo "|                 1.16.9                 |"
+echo "|                 1.17.0                 |"
+echo "|             1.17.1 - latest            |"
+echo "|========================================|"
+echo "                                          "
+##
+read -p "     Enter the version name: " ver
+echo "                                          "
+##
+echo "|========================================|"
+echo "| Ok, you have chosen the $ver version |  "
+echo "|               Installing...            |"
+echo "|========================================|"
+##
+clear
+##
+
 ## Section for Debian
 
 if grep 'Debian GNU/Linux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
@@ -6,7 +49,7 @@ if grep 'Debian GNU/Linux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-rel
 then
 
 	apt update && apt upgrade -y
-	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
 	chmod +x gitea
 	adduser \
 	   --system \
@@ -36,7 +79,7 @@ if grep 'Fedora Linux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
 then
 
 	dnf update -y && dnf install wget -y
-	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
 	chmod +x gitea
 	adduser \
 	   --shell /bin/bash \
@@ -62,7 +105,7 @@ if grep 'AlmaLinux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
 then
 
 	dnf update -y && dnf install wget -y
-	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
 	chmod +x gitea
 	adduser \
 	   --shell /bin/bash \
@@ -88,7 +131,7 @@ if grep 'CentOS' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
 then
 
 	dnf update -y && dnf install wget -y
-	wget -O gitea https://dl.gitea.io/gitea/1.17.1/gitea-1.17.1-linux-amd64
+	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
 	chmod +x gitea
 	adduser \
 	   --shell /bin/bash \
@@ -106,3 +149,29 @@ then
 	systemctl start gitea
 
 fi
+
+##
+clear
+##
+echo "                                                    "                                               
+echo "      @%%%%%%%%%%%%%%%%%%%%%%%%+-%%%%%%%%%%%%%%%%%% "
+echo "    @%%%%%%%%%%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% "
+echo "   %%%%     @%%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% "
+echo "   %%%       %%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% "
+echo "  @%%%       %%%%%%%%%%%%%%+--++.%%%%%%%%%%%%%%%%%% "
+echo "   %%%       @%%%%%%%%%%%%*   .=--+%%%%%%%%%%%%%%%@ "
+echo "   %%%%       %%%%%%%%%%%*   +%=     .*%%%%%%%%%%%  "
+echo "    %%%%%      %%%%%%%%%+   .=:.:=*    -%%%%%%%%%@  "
+echo "      %%%%%%%%@%%%%%%%%+   .=-   .=-  .=%%%%%%%%%   "
+echo "        @%%%%%%%%%%%%%+   +%+   .=:  .=%%%%%%%%%    "
+echo "                @%%%%%%:. .:.  *%+  .=%%%%%%%%%     "
+echo "                  %%%%%%%%=:.  -+- .=%%%%%%%%%@     "
+echo "                   %%%%%%%%%%%=*. .=%%%%%%%%%       "
+echo "                    @%%%%%%%%%%%%%%%%%%%%%%%        "
+echo "                       %%%%%%%%%%%%%%%%%%@          "
+echo "                                                    "
+echo "                                                    "
+echo "            Thank you for using my script!          "
+echo "           Visit http://ip_your_server:3000         "
+echo "      For further installation and configuration    "
+echo "                                                    "
