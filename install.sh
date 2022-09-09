17686557c95417f03faaa4713790a2bba0dbbd46
WolfAURman <furry.wolf.alone13@gmail.com>
2022-09-10 00:21:41 +0600

    Optimizing the installer
    Removing unnecessary code to improve reading and future script updates

--- install.sh
+++ install.sh
@@ -1,46 +1,56 @@
 #!/bin/bash
-##
+
 clear
-##
-echo "|========================================|"
-echo "|               Hey! Hello!              |"
-echo "|   Have you decided to install gitea    |"
-echo "|             on your server             |"
-echo "|  This script will help you with this   |"
-echo "|         Answer a few questions         |"
-echo "|   and then wait for the installation   |"
-echo "|========================================|"
-echo "|             Successful use!            |"
-echo "|========================================|"
-echo "|   The installation will start in 10s   |"
-echo "|========================================|"
-##
-sleep 10
-##
-clear
-##
-echo "|========================================|"
-echo "|  Which version do you want to install? |"
-echo "|      These versions are available:     |"
-echo "|========================================|"
-echo "|                 1.16.7                 |"
-echo "|                 1.16.8                 |"
-echo "|                 1.16.9                 |"
-echo "|                 1.17.0                 |"
-echo "|             1.17.1 - latest            |"
-echo "|========================================|"
-echo "                                          "
-##
+
+function init () {
+    echo "
+|========================================|
+|               Hey! Hello!              |
+|   Have you decided to install gitea    |
+|             on your server             |
+|  This script will help you with this   |
+|         Answer a few questions         |
+|   and then wait for the installation   |
+|========================================|
+|             Successful use!            |
+|========================================|
+|   The installation will start in 10s   |
+|========================================|"
+sleep 10 && clear
+}
+
+init
+
+function init1 () {
+    echo "
+|========================================|
+|  Which version do you want to install? |
+|      These versions are available:     |
+|========================================|
+|                 1.16.8                 |
+|                 1.16.9                 |
+|                 1.17.0                 |
+|                 1.17.1                 |
+|             1.17.2 - latest            |
+|========================================|
+"
+}
+
+init1
+
 read -p "     Enter the version name: " ver
-echo "                                          "
-##
-echo "|========================================|"
-echo "| Ok, you have chosen the $ver version |  "
-echo "|               Installing...            |"
-echo "|========================================|"
-##
-clear
-##
+
+function init2 () {
+   clear
+    echo "
+|========================================|
+| Ok, you have chosen the $ver version |  
+|               Installing...            |
+|========================================|
+"
+}
+
+init2
 
 ## Section for Debian
 
@@ -67,18 +77,17 @@ then
 	chmod 770 /etc/gitea
 	cp gitea /usr/local/bin/gitea
 	cp gitea.service /etc/systemd/system
-	systemctl enable gitea
-	systemctl start gitea
+	systemctl enable --now gitea
 
 fi
 
-## Section for Fedora
+## Section for RHEL and its forks
 
-if grep 'Fedora Linux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
+if ls /etc/redhat-release > /dev/null 2>&1
 
 then
 
-	dnf update -y && dnf install wget -y
+	dnf install wget -y
 	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
 	chmod +x gitea
 	adduser \
@@ -93,85 +102,34 @@ then
 	chmod 770 /etc/gitea
 	cp gitea /usr/local/bin/gitea
 	cp gitea.service /etc/systemd/system
-	systemctl enable gitea
-	systemctl start gitea
+	systemctl enable --now gitea
 
 fi
 
-## Section for AlmaLinux
+function init3 () {
+   clear
+    echo "                                              
+      @%%%%%%%%%%%%%%%%%%%%%%%%+-%%%%%%%%%%%%%%%%%% 
+    @%%%%%%%%%%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% 
+   %%%%     @%%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% 
+   %%%       %%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% 
+  @%%%       %%%%%%%%%%%%%%+--++.%%%%%%%%%%%%%%%%%% 
+   %%%       @%%%%%%%%%%%%*   .=--+%%%%%%%%%%%%%%%@ 
+   %%%%       %%%%%%%%%%%*   +%=     .*%%%%%%%%%%%  
+    %%%%%      %%%%%%%%%+   .=:.:=*    -%%%%%%%%%@  
+      %%%%%%%%@%%%%%%%%+   .=-   .=-  .=%%%%%%%%%   
+        @%%%%%%%%%%%%%+   +%+   .=:  .=%%%%%%%%%    
+                @%%%%%%:. .:.  *%+  .=%%%%%%%%%     
+                  %%%%%%%%=:.  -+- .=%%%%%%%%%@     
+                   %%%%%%%%%%%=*. .=%%%%%%%%%       
+                    @%%%%%%%%%%%%%%%%%%%%%%%        
+                       %%%%%%%%%%%%%%%%%%@          
+                                                    
+                                                    
+            Thank you for using my script!          
+           Visit http://ip_your_server:3000         
+      For further installation and configuration    
+"
+}
 
-if grep 'AlmaLinux' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
-
-then
-
-	dnf update -y && dnf install wget -y
-	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
-	chmod +x gitea
-	adduser \
-	   --shell /bin/bash \
-	   --home /home/git \
-	   git
-	mkdir -p /var/lib/gitea/{custom,data,log}
-	chown -R git:git /var/lib/gitea/
-	chmod -R 750 /var/lib/gitea/
-	mkdir /etc/gitea
-	chown root:git /etc/gitea
-	chmod 770 /etc/gitea
-	cp gitea /usr/local/bin/gitea
-	cp gitea.service /etc/systemd/system
-	systemctl enable gitea
-	systemctl start gitea
-
-fi
-
-## Section for CentOS
-
-if grep 'CentOS' /etc/os-release > /dev/null 2>&1 #NAME in /etc/os-release
-
-then
-
-	dnf update -y && dnf install wget -y
-	wget -O gitea https://dl.gitea.io/gitea/$ver/gitea-$ver-linux-amd64
-	chmod +x gitea
-	adduser \
-	   --shell /bin/bash \
-	   --home /home/git \
-	   git
-	mkdir -p /var/lib/gitea/{custom,data,log}
-	chown -R git:git /var/lib/gitea/
-	chmod -R 750 /var/lib/gitea/
-	mkdir /etc/gitea
-	chown root:git /etc/gitea
-	chmod 770 /etc/gitea
-	cp gitea /usr/local/bin/gitea
-	cp gitea.service /etc/systemd/system
-	systemctl enable gitea
-	systemctl start gitea
-
-fi
-
-##
-clear
-##
-echo "                                                    "                                               
-echo "      @%%%%%%%%%%%%%%%%%%%%%%%%+-%%%%%%%%%%%%%%%%%% "
-echo "    @%%%%%%%%%%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% "
-echo "   %%%%     @%%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% "
-echo "   %%%       %%%%%%%%%%%%%%%%%%*.%%%%%%%%%%%%%%%%%% "
-echo "  @%%%       %%%%%%%%%%%%%%+--++.%%%%%%%%%%%%%%%%%% "
-echo "   %%%       @%%%%%%%%%%%%*   .=--+%%%%%%%%%%%%%%%@ "
-echo "   %%%%       %%%%%%%%%%%*   +%=     .*%%%%%%%%%%%  "
-echo "    %%%%%      %%%%%%%%%+   .=:.:=*    -%%%%%%%%%@  "
-echo "      %%%%%%%%@%%%%%%%%+   .=-   .=-  .=%%%%%%%%%   "
-echo "        @%%%%%%%%%%%%%+   +%+   .=:  .=%%%%%%%%%    "
-echo "                @%%%%%%:. .:.  *%+  .=%%%%%%%%%     "
-echo "                  %%%%%%%%=:.  -+- .=%%%%%%%%%@     "
-echo "                   %%%%%%%%%%%=*. .=%%%%%%%%%       "
-echo "                    @%%%%%%%%%%%%%%%%%%%%%%%        "
-echo "                       %%%%%%%%%%%%%%%%%%@          "
-echo "                                                    "
-echo "                                                    "
-echo "            Thank you for using my script!          "
-echo "           Visit http://ip_your_server:3000         "
-echo "      For further installation and configuration    "
-echo "                                                    "
+init3
